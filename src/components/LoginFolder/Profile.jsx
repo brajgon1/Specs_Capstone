a489953b5d05458bd6b88dcb665a36286bffb6cc
Brandon Gonzalez <brajgon@gmail.com>
2024-07-16 19:09:17 -0600

    additions to profile page

--- src/components/LoginFolder/Profile.jsx
+++ src/components/LoginFolder/Profile.jsx
@@ -1,13 +1,14 @@
 const Profile = () => {
-    return (
-        <div>
-            <div className="profile-header">
-                <img src="https://via.placeholder.com/150" alt="Profile Pic" />
-                <h1 className="username">{}</h1>
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="profile-header">
+        <img src="https://via.placeholder.com/150" alt="Profile Pic" className="pfp" />
+        <h1 className="username">{}</h1>
+      </div>
+      <div className="favorite-movie-list">{}</div>
+      <div className="recent-activity-feed">{}</div>
+    </div>
+  );
+};
 
 export default Profile;
