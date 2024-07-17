787afbb51b747839162b8c40433f80ad1b622b6c
Brandon Gonzalez <brajgon@gmail.com>
2024-07-16 19:07:32 -0600

    outline for Profile page

---
+++ src/components/LoginFolder/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+    return (
+        <div>
+            <div className="profile-header">
+                <img src="https://via.placeholder.com/150" alt="Profile Pic" />
+                <h1 className="username">{}</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
