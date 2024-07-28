347b86805f60c3c9a148701491c4d1c5899db498
Brandon Gonzalez <brajgon@gmail.com>
2024-07-28 12:10:21 -0600

    updating the movie card

--- src/components/Movies/MovieCard.jsx
+++ src/components/Movies/MovieCard.jsx
@@ -4,46 +4,75 @@ import Rating from "../Rating/Rating";
 import './MovieCard.css';
 
 const MovieCard = () => {
+    const [modalOpen, setModalOpen ] = useState(false);
+    const [rating, setRating] = useState(movie.vote_average);
+    const [watchlist, setWatchlist] = useState([]);
+    const [favorites, setFavorites] = useState([]);
 
-    const [movie, setMovie] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchMovie = async () => {
-         try {
-                const apiKey = process.env.REACT_APP_API_KEY;
-                const response = await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`);
-                setMovie(response.data);
-                setLoading(false);
-            } catch (err) {
-                setError(err);
-                setLoading(false);
-            }
-        };
-
-        fetchMovie();
-    }, []);
-
-    if (loading) {
-        return <div>Loading...</div>;
+    const toggleModal = () => {
+        setModalOpen(!modalOpen);
     }
 
-    if (error) {
-        return <div>Error fetching movie data</div>;
+    const addToWatchlist = () => {
+        setWatchlist([...watchlist, movie]);
+        alert(`${movie.title} added to watchlist!`);
     }
 
+    const addToFavorites = () => {
+        if (favorites.length < 4) {
+            setFavorites([...favorites, movie]);
+            alert(`${movie.title} added to favorites!`);
+        } else {
+            alert("Maximum number of favorites reached!");
+        }
+    }
 
-    return(
-        <div>
-            <div className="movie-card">
-            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="Movie Poster" />
-                <h3>{movie.title}</h3>
-                <p>Rating: <Rating rating={movie.vote_average} /></p>
-            </div>
+    const handleRating = (newRating) => {
+        setRating(newRating);
+        alert(`${movie.title} rated ${newRating} stars!`);
+    }
 
-        </div>
-    )
+//     useEffect(() => {
+//         const fetchMovie = async () => {
+//          try {
+//                 const apiKey = process.env.REACT_APP_API_KEY;
+//                 const response = await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`);
+//                 setMovie(response.data);
+//                 setLoading(false);
+//             } catch (err) {
+//                 setError(err);
+//                 setLoading(false);
+//             }
+//         };
+
+//         fetchMovie();
+//     }, []);
+
+//     if (loading) {
+//         return <div>Loading...</div>;
+//     }
+
+//     if (error) {
+//         return <div>Error fetching movie data</div>;
+//     }
+
+
+//     return(
+//         <div>
+//             <div id="myModal" className="modal">
+//             <span id="close">X</span>
+//             <img className="modal-content modalImg"/>
+//             </div>
+//             <div className="movie-card">
+//             <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="Movie Poster" />
+//                 <h3>{movie.title}</h3>
+//                 <p>Rating: <Rating rating={movie.vote_average} /></p>
+//             </div>
+
+//         </div>
+//     )
+// }
 }
 
 export default MovieCard;
